444cc2a58fa7280e6a1425dd1222a6d8332be13e
JAEYOUNGYUN <lastmore9@gmail.com>
2020-12-03 15:29:43 +0900

    refactor: Implement BaseViewModel and ViewModelBindableType which are base for viewmodel and view.

---
+++ Olib/Modules/Common/BaseViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewModel.swift
+//  Olib
+//
+//  Created by jaeyoung Yun on 2020/12/03.
+//
+
+import Foundation
+import RxSwift
+import NSObject_Rx
+
+class BaseViewModel: HasDisposeBag {
+    
+}
---
+++ Olib/Modules/Common/ViewModelBindableType.swift
@@ -0,0 +1,26 @@
+//
+//  ViewModelBindableType.swift
+//  Olib
+//
+//  Created by jaeyoung Yun on 2020/12/03.
+//
+
+import UIKit
+
+protocol ViewModelBindableType {
+    
+    associatedtype ViewModelType: BaseViewModel
+    
+    var viewModel: ViewModelType { get set }
+    
+    func bindUI()
+}
+
+extension ViewModelBindableType where Self: UIViewController {
+    
+    mutating func bind(viewModel: Self.ViewModelType) {
+        self.viewModel = viewModel
+        
+        bindUI()
+    }
+}
