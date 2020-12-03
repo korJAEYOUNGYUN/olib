ae4d59c8174dd4aed3dc0a0efd1b87048b1d6632
JAEYOUNGYUN <lastmore9@gmail.com>
2020-12-03 13:16:08 +0900

    refactor: Add parsing book from json data.

--- Olib/Models/Book.swift
+++ Olib/Models/Book.swift
@@ -14,4 +14,26 @@ struct Book: Codable {
     let location: String
     let is_available: Bool
     let book_info: BookInfo
+    
+    // for list
+    static func parse(data: Data) -> [Book] {
+        do {
+            let bookList = JSONDecoder().decode([Book].self, from: data)
+        } catch {
+            print(error)
+        }
+        
+        return bookList
+    }
+    
+    // for retrieve
+    static func parse(data: Data) -> Book {
+        do {
+            let book = JSONDecoder().decode(Book.self, from: data)
+        } catch {
+            print(error)
+        }
+        
+        return book
+    }
 }
