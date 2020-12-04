46fbc8a7aa8c5fb2c194ef65af9a6ebbc7152c37
JAEYOUNGYUN <lastmore9@gmail.com>
2020-12-04 20:56:21 +0900

    refactor: Remove deprecated in SignInViewController and Main.storyboard.
    Removed IBAction when tap sign in button.

--- Olib/Modules/SignIn/SignInViewController.swift
+++ Olib/Modules/SignIn/SignInViewController.swift
@@ -72,42 +72,7 @@ class SignInViewController: UIViewController, ViewModelBindableType {
             })
             .disposed(by: rx.disposeBag)
     }
-    
-    @IBAction func didPresssignInButton(_ sender: UIButton) {
-//        let email = emailTextField.text!
-//        let password = passwordTextField.text!
-//
-//    private func authenticate(with email: String, password: String) {
-//
-//        AuthManager.shared.authenticate(with: email, password: password, completion: handleAuthenticationResponse)
-//    }
-//
-//    func handleAuthenticationResponse(_ response: HTTPURLResponse) {
-//
-//        switch response.statusCode {
-//        case 200:
-//            DispatchQueue.main.async {
-//                let menuTabController = MenuTabBarController()
-//                menuTabController.modalPresentationStyle = .fullScreen
-//                self.present(menuTabController, animated: true, completion: nil)
-//            }
-//        case 401:
-//            // alert no account with the given credentials
-//            DispatchQueue.main.async {
-//                let alert = UIAlertController(title: "Signin Failed", message: "There is no user matched from given credentials.", preferredStyle: .alert)
-//                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-//                self.present(alert, animated: true, completion: nil)
-//            }
-//        default:
-//            // server error
-//            DispatchQueue.main.async {
-//                let alert = UIAlertController(title: "Network error", message: "Please try it later.", preferredStyle: .alert)
-//                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-//                self.present(alert, animated: true, completion: nil)
-//            }
-//        }
-    }
-    
+        
     @IBAction func goToSignUp(_ sender: Any) {
         // go further to signupVC
         let signUpVC = UIStoryboard(name: "SignUpViewController", bundle: nil).instantiateViewController(withIdentifier: "SignUpViewController")
