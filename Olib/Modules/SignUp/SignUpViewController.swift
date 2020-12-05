4b620d8477c646096b6b78bdd94b4254609b9618
JAEYOUNGYUN <lastmore9@gmail.com>
2020-12-05 19:43:54 +0900

    refactor: Seperate logics in SignUpViewController to SignUpViewModel and adjust reactive.
    Replaced IBActions to rx and implemented viewmodel that handles logics of signing up.

--- Olib/Modules/SignUp/SignUpViewController.swift
+++ Olib/Modules/SignUp/SignUpViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, ViewModelBindableType {
 
+    var viewModel: SignUpViewModel!
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var emailWarningLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -16,110 +20,81 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var password2TextField: UITextField!
     @IBOutlet weak var password2WarningLabel: UILabel!
     @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        emailWarningLabel.isHidden = true
-        passwordWarningLabel.isHidden = true
-        password2WarningLabel.isHidden = true
-    }
-    
-    @IBAction func didPressSignUpButton(_ sender: Any) {
-        guard checkEmailTextField(emailTextField), checkPasswordTextField(passwordTextField), checkPasswordTextField(password2TextField) else {
-            return
-        }
         
-        let email = emailTextField.text!
-        let password = passwordTextField.text!
-        let password2 = password2TextField.text!
-        if password != password2 {
-            let alert = UIAlertController(title: "Password not equal", message: "Password1 and password2 are not equal.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            
-            return
-        }
-
-        signUp(with: email, password: password)
+        emailWarningLabel.text = String.EMAIL_INVALID
+        passwordWarningLabel.text = String.PASSWORD_INVALID
+        password2WarningLabel.text = String.PASSWORD_INVALID
+        
+        // FIXME: for test
+        viewModel = SignUpViewModel()
+        bindUI()
     }
     
-    private func signUp(with email: String, password: String) {
-        startLoading()
-        AuthManager.shared.signUp(with: email, password: password, completion: handleSignUpResponse)
+    func bindUI() {
+        getText(from: emailTextField.rx.text)
+            .bind(to: viewModel.email)
+            .disposed(by: rx.disposeBag)
+        
+        getText(from: passwordTextField.rx.text)
+            .bind(to: viewModel.password)
+            .disposed(by: rx.disposeBag)
+        
+        getText(from: password2TextField.rx.text)
+            .bind(to: viewModel.password2)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.emailValidation
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { isValid in
+                UIView.animate(withDuration: 0.5) { [weak self] in
+                    self?.emailWarningLabel.isHidden = isValid
+                }
+            })
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.passwordValidation
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { isValid in
+                UIView.animate(withDuration: 0.5) { [weak self] in
+                    self?.passwordWarningLabel.isHidden = isValid
+                }
+            })
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.password2Validation
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { isValid in
+                UIView.animate(withDuration: 0.5) { [weak self] in
+                    self?.password2WarningLabel.isHidden = isValid
+                }
+            })
+            .disposed(by: rx.disposeBag)
+        
+        Observable.combineLatest(viewModel.emailValidation, viewModel.passwordMatch) { $0 && $1 }
+            .bind(to: signUpButton.rx.isEnabled)
+            .disposed(by: rx.disposeBag)
+        
+        signUpButton.rx.tap
+            .map { [unowned self] _ -> (String, String) in (self.emailTextField.text!, self.passwordTextField.text!) }
+            .subscribe(onNext: { [weak self] in
+                self?.viewModel.signUp(with: $0, password: $1)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.viewModel.cancel()
+            })
+            .disposed(by: rx.disposeBag)
     }
     
-    private func handleSignUpResponse(_ response: HTTPURLResponse) {
-        stopLoading()
-        switch response.statusCode {
-        case 201:
-            DispatchQueue.main.async {
-                let menuTabController = MenuTabBarController()
-                menuTabController.modalPresentationStyle = .fullScreen
-                self.present(menuTabController, animated: true, completion: nil)
-            }
-        case 400:
-            DispatchQueue.main.async {
-                let alert = UIAlertController(title: "Email already exists", message: "A user with that email already exists.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-            }
-        default:
-            DispatchQueue.main.async {
-                let alert = UIAlertController(title: "Network error", message: "Please try it later.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-            }
-        }
-    }
-    
-    @IBAction func cancel(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func emailEditingChanged(_ sender: UITextField) {
-        _ = checkEmailTextField(sender)
-    }
-    
-    @IBAction func passwordEditingChanged(_ sender: UITextField) {
-        _ = checkPasswordTextField(sender)
-    }
-    
-    private func checkEmailTextField(_ textField: UITextField) -> Bool {
-        if textField.text!.isValidEmail() {
-            hideWarning(label: emailWarningLabel)
-            return true
-        } else {
-            showWarning(label: emailWarningLabel, msg: .EMAIL_INVALID)
-            return false
-        }
-    }
-    
-    private func checkPasswordTextField(_ textField: UITextField) -> Bool {
-        if textField.text!.isValidPassword() {
-            if textField == passwordTextField {
-                hideWarning(label: passwordWarningLabel)
-            } else {
-                hideWarning(label: password2WarningLabel)
-            }
-            return true
-        } else {
-            if textField == password2TextField {
-                showWarning(label: passwordWarningLabel, msg: .PASSWORD_INVALID)
-            } else {
-                showWarning(label: password2WarningLabel, msg: .PASSWORD_INVALID)
-            }
-            return false
-        }
-    }
-    
-    private func startLoading() {
-        signUpButton.isEnabled = false
-    }
-    
-    private func stopLoading() {
-        DispatchQueue.main.async {
-            self.signUpButton.isEnabled = true
-        }
+    func getText(from text: ControlProperty<String?>) -> Observable<String> {
+        text.orEmpty
+            .debounce(.milliseconds(300), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
     }
 }
---
+++ Olib/Modules/SignUp/SignUpViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  SignUpViewModel.swift
+//  Olib
+//
+//  Created by jaeyoung Yun on 2020/12/05.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+class SignUpViewModel: BaseViewModel {
+    
+    let email = PublishRelay<String>()
+    let emailValidation = BehaviorRelay(value: false)
+    
+    let password = PublishRelay<String>()
+    let passwordValidation = BehaviorRelay(value: false)
+    
+    let password2 = PublishRelay<String>()
+    let password2Validation = BehaviorRelay(value: false)
+    
+    let passwordMatch = BehaviorRelay(value: false)
+    
+    override init() {
+        super.init()
+        
+        email
+            .map { $0.isValidEmail() }
+            .bind(to: emailValidation)
+            .disposed(by: disposeBag)
+        
+        password
+            .map { $0.isValidPassword() }
+            .bind(to: passwordValidation)
+            .disposed(by: disposeBag)
+        
+        password2
+            .map { $0.isValidPassword() }
+            .bind(to: password2Validation)
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(passwordValidation, password2Validation, password, password2) { $0 && $1 && $2 == $3 }
+            .bind(to: passwordMatch)
+            .disposed(by: disposeBag)
+    }
+    
+    func signUp(with email: String, password: String) {
+        AuthManager.shared.rxSignUp(with: email, password: password)
+            .subscribe { (response) in
+                print(response)
+            } onError: { (error) in
+                print(error)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    // TODO: dismiss signup view
+    func cancel() {
+        print("cancel")
+    }
+}
